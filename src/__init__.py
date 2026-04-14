e5edc7f391bbfc14bd04b0f62ae14611a7bbad42
Sangam Shikerkar <sangamshikerkar1@gmail.com>
2026-04-14 23:55:09 +0530

    setup and req add

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Sangam Shikerkar',
+author_email='sangamshikerkar1@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','numoy']
+
+)
